13a9ca2d9458247ee8957bb4047eb0d9098dc302
0xmav <0xmav.eth@gmail.com>
2022-12-11 12:50:04 +0800

    Fixed the LocalStorage issue?

--- pages/_app.js
+++ pages/_app.js
@@ -2,21 +2,31 @@ import "../styles/normalize.css";
 import "../styles/index.css";
 import Layout from "../components/Layout/layout";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
-import { WalletContext, beaconWallet } from "../lib/wallet";
+import { WalletContext, beaconWallet, Tezos } from "../lib/wallet";
 import { SessionContextProvider } from "@supabase/auth-helpers-react";
 import supabase from "../lib/supabase";
 
 import { AnimatePresence } from "framer-motion";
 
 function MyApp({ Component, pageProps }) {
-    const [wallet] = useState(beaconWallet);
+    const [wallet, setWallet] = useState(beaconWallet);
+
+    useEffect(()=>{
+        (async () => {
+            if (wallet.storage === undefined) {
+              const _wallet = new (await import("@taquito/beacon-wallet")).BeaconWallet({ name: "DeCoFi" });
+              setWallet(_wallet)
+              Tezos.setWalletProvider(_wallet);
+            }
+          })();
+    },[]);
 
     return (
         <>
             <SessionContextProvider supabaseClient={supabase} initialSession={pageProps.initialSession}>
-                <WalletContext.Provider value={wallet}>
+                <WalletContext.Provider value={ wallet }>
                     <AnimatePresence mode="wait" initial={false}>
                         <Layout>
                             <Component {...pageProps} />
